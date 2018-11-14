0e76fe61d75b794e11ebbc50ed659d3a8bb65238
Germán <german.reyga@gmail.com>
2018-11-14 01:15:20 -0600

    Queries y modulo del calendario terminados

--- routes/calendario.js
+++ routes/calendario.js
@@ -40,31 +40,51 @@ module.exports = {
 
     var dayDiff = daysUpTo(currentDate, selectedDate);
 
-    var current_user = req.session.userid;
-    var query =
-      'SELECT * ' +
-      'FROM receta a ' +
-      'INNER JOIN medicina b ON a.id_Medicina = b.id_Medicina ' +
-      'INNER JOIN presentacion_medicina c ON b.tipoMedicina  = c.id_Presentacion ' +
-      'WHERE a.username_Paciente = ? AND a.diasDosis>?';
+    // Si el dia de diferencia es menor o igual a cero (se eligio uno dia antes del actual), no saldra ninguna receta
+    if (dayDiff > 0) {
+      var current_user = req.session.userid;
+      var query =
+        'SELECT * ' +
+        'FROM receta a ' +
+        'INNER JOIN medicina b ON a.id_Medicina = b.id_Medicina ' +
+        'INNER JOIN presentacion_medicina c ON b.tipoMedicina  = c.id_Presentacion ' +
+        'WHERE a.username_Paciente = ? AND a.diasDosis>?';
 
-    connection.query(query, [current_user, dayDiff], function(
-      error,
-      results,
-      fields
-    ) {
-      if (error) {
-        return res.status(500).send(error);
-      } else {
-        console.log(results);
-        /*res.render('modulo-calendario.ejs', {
-          listaMedicinas: results,
-          titulo: 'Panel de calendario',
-          status: true,
-          message: 'Éxito'
-        });*/
-      }
-    });
+      connection.query(query, [current_user, dayDiff], function(
+        error,
+        results,
+        fields
+      ) {
+        if (error) {
+          return res.status(500).send(error);
+        } else {
+          res.render('dia-elegido.ejs', {
+            listaMedicinas: results,
+            dia:
+              selectedDate.getDay() +
+              '/' +
+              selectedDate.getMonth() +
+              '/' +
+              selectedDate.getFullYear(),
+            titulo: 'Agenda de día',
+            status: true,
+            message: 'Éxito'
+          });
+        }
+      });
+    } else {
+      res.render('dia-elegido.ejs', {
+        dia:
+          selectedDate.getDay() +
+          '/' +
+          selectedDate.getMonth() +
+          '/' +
+          selectedDate.getFullYear(),
+        titulo: 'Agenda de día',
+        status: false,
+        message: 'No éxito'
+      });
+    }
   }
 };
 
